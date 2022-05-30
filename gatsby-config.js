cb0c49ba79b82791cefb51a9ddb810ed6dfd7822
James Fincher <heartandkarma@gmail.com>
2022-05-30 13:06:02 -0700

    scaffolded from old fruit-stand commit

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-netlify-cms`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/assets/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+  ],
+}
